4918e6c60be2f07fa888dff32bdd161dd8e38922
Robert Duffner <robert@findcanary.com>
2019-08-19 22:43:52 +0300

    Initial commit

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Find Canary, LLC. All rights reserved.
+ */
+declare(strict_types = 1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'FindCanary_SubscribeProTrial', __DIR__);
